780129198121c13500f535d52bdda6b10548262a
miguel jimenez <denkatalabeno@gmail.com>
2018-05-31 14:25:13 +0200

    generated all pages;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { LibraryPage } from './../pages/library/library';
+import { QuotePage } from './../pages/quote/quote';
+import { FavoritePage } from './../pages/favorite/favorite';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { QuotesPage } from '../pages/quotes/quotes';
+import { Quote } from '@angular/compiler';
+import { SettingsPage } from '../pages/settings/settings';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    QuotesPage,
+    FavoritePage,
+    QuotePage,
+    LibraryPage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +29,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    QuotesPage,
+    FavoritePage,
+    QuotePage,
+    LibraryPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
