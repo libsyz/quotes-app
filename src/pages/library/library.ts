a25e936a124fe084a94cdf8323ac2fd4e029ac13
miguel jimenez <denkatalabeno@gmail.com>
2018-05-31 18:09:53 +0200

    added quotes library pages

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quotes.interface';
+import quotes from '../../data/quotes';
+import { QuotesPage } from '../quotes/quotes';
 
 /**
+ * 
  * Generated class for the LibraryPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
@@ -13,13 +17,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-library',
   templateUrl: 'library.html',
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit {
+  quoteCollection: {category: string, quotes: Quote[], icon: string}[];
+  quotesPage = QuotesPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ngOnInit() {
+    this.quoteCollection = quotes;
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
-
 }
--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the QuotesPage page.
@@ -13,13 +13,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class QuotesPage implements OnInit {
+  quoteGroup: any;
+  allQuotes: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  private alertCtrl: AlertController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+  ngOnInit() {
+
+    this.quoteGroup = this.navParams.data;
+    this.allQuotes = this.quoteGroup.quotes;
   }
+  
+  addToFavorite(quote: any) {
+    const alert = this.alertCtrl.create({
+      title: "Add Quote",
+      subTitle: "Are you sure?",
+      message: "just wanted to add something else",
+      buttons: [{ 
+                  text:'Yes',
+                  handler: ()=>{
+                    console.log("ok!");
+                  }
+                   },
+                  {
+                  text: 'Nope',
+                  handler: ()=> {
+                    console.log("won't do it!");
+                  }                          
+                }]
+    });
+
+    alert.present();
+  }
+
 
 }
